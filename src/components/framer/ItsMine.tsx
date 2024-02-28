c549c3d0880b1ca08e7d4d0ed0262401d2393d2b
ian <38205068+im-ian@users.noreply.github.com>
2024-02-28 17:41:07 +0900

    Feature: add it's mine

---
+++ src/components/framer/ItsMine.tsx
@@ -0,0 +1,117 @@
+import { forwardRef, useEffect, useState } from "react";
+import { delay, motion, useAnimate } from "framer-motion";
+
+import { Centered } from "../layout";
+import { CHIPMUNK } from "../../constants/symbol";
+
+interface ButtonProps {
+  onClick?: () => void;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ onClick }, ref) => {
+    return (
+      <motion.button
+        ref={ref}
+        style={{
+          padding: "0 16px",
+          lineHeight: "40px",
+        }}
+        onClick={onClick}
+      >
+        click me!
+      </motion.button>
+    );
+  }
+);
+
+const Animation = () => {
+  const [buttonScope, buttonAnimate] = useAnimate();
+  const [chipmunkScope, chipmunkAnimate] = useAnimate();
+
+  useEffect(() => {
+    async function startAnimate() {
+      await chipmunkAnimate(chipmunkScope.current, { x: 100 }, { duration: 1 });
+      await chipmunkAnimate(
+        chipmunkScope.current,
+        { y: -20 },
+        { duration: 0.2 }
+      );
+      await chipmunkAnimate(chipmunkScope.current, { y: 0 }, { duration: 0.2 });
+      await chipmunkAnimate(
+        chipmunkScope.current,
+        { x: 0 },
+        { delay: 0.5, ease: "easeOut" }
+      );
+
+      async function moveButton(x: number) {
+        return Promise.all([
+          chipmunkAnimate(
+            chipmunkScope.current,
+            { x },
+            { delay: 0.3, duration: 1 }
+          ),
+          buttonAnimate(
+            buttonScope.current,
+            { x },
+            { delay: 0.3, duration: 1 }
+          ),
+        ]);
+      }
+
+      await moveButton(40);
+      await moveButton(80);
+      await moveButton(120);
+      await moveButton(160);
+      await moveButton(200);
+      await moveButton(240);
+      await moveButton(280);
+    }
+
+    startAnimate();
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+      }}
+    >
+      <Button ref={buttonScope} />
+      <motion.span
+        ref={chipmunkScope}
+        style={{
+          position: "absolute",
+          fontSize: 34,
+        }}
+        initial={{
+          x: 200,
+        }}
+      >
+        {CHIPMUNK}
+      </motion.span>
+    </div>
+  );
+};
+
+function ItsMine() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <Centered
+      style={{
+        width: "100%",
+        padding: "15px 0",
+        textAlign: "center",
+        overflowX: "hidden",
+        overflowY: "visible",
+      }}
+    >
+      {!isClicked && <Button onClick={() => setIsClicked(true)} />}
+      {isClicked && <Animation />}
+    </Centered>
+  );
+}
+
+export default ItsMine;
--- src/constants/gallery.tsx
+++ src/constants/gallery.tsx
@@ -1,5 +1,6 @@
 import ChipmunkInTheBox from "../components/framer/InTheBox";
 import Balance from "../components/framer/Balance";
+import ItsMine from "../components/framer/ItsMine";
 
 import { Gallery } from "../types/gallery";
 
@@ -17,5 +18,12 @@ export const GALLERY_ITEM: Gallery = [
     description: "하얀 막대를 위 아래로 드래그해보세요.",
     icon: "⚖️",
     content: <Balance />,
+  },
+  {
+    id: "its-mine",
+    title: "이건 내꺼야",
+    description: "버튼을 클릭해주세요.",
+    icon: "😼",
+    content: <ItsMine />,
   },
 ];
